b858d995f61b1cad6e3a82a8c51788fa39d36041
garlics93 <alexhall93@me.com>
2016-06-14 14:14:00 +0100

    Status relations completed

--- app/Status.php
+++ app/Status.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    //
+    // Users who have used this status
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -70,4 +70,10 @@ class User extends Authenticatable
     {
         return $this->belongsTo('App\Network', 'creator_id');
     }
+
+    // The statuses this user has used
+    public function statuses()
+    {
+        return $this->belongsToMany('App\Status');
+    }
 }
