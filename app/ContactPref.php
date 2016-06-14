8db17035e7a74e1b3d67dd489f433d2817489ed6
garlics93 <alexhall93@me.com>
2016-06-14 10:25:18 +0100

    Added contact preferences relations

--- app/ContactPref.php
+++ app/ContactPref.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactPref extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function object()
+    {
+        return $this->morphTo();
+    }
 }
