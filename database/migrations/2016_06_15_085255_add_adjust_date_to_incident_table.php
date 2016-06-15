5846dc343cbbdb18f53f2abf9581fa3206a443e2
garlics93 <alexhall93@me.com>
2016-06-15 09:57:44 +0100

    Adds date column to incidents table

---
+++ database/migrations/2016_06_15_085255_add_adjust_date_to_incident_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdjustDateToIncidentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('incidents', function (Blueprint $table) {
+            $table->date('date')->nullable()->default(null)->after('dets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('incidents', function (Blueprint $table) {
+            //
+        });
+    }
+}
