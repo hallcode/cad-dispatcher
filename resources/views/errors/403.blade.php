29a927acb43ecf0de7acd7ded91c357f0002c581
garlics93 <alexhall93@me.com>
2016-06-15 21:17:48 +0100

    Created error pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+<i class="fa fa-exclamation"></i> Page restricted
+@endsection
+
+@section('content')
+<p>
+    It looks like you don't have permission to view this item.
+</p>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title')
+<i class="fa fa-exclamation"></i> Page not found
+@endsection
+
+@section('content')
+<p>
+    Sorry, but it looks like whatever you were looking for isn't here.
+</p>
+<p>
+    It may have been moved or deleted. If you were expecting an incident, it may have been resulted.
+</p>
+@endsection
