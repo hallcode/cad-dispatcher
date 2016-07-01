6f7dc51b7c31d4ba1f932ea182c1332813b8945b
garlics93 <alexhall93@me.com>
2016-07-01 20:29:45 +0100

    Add Mapper aliases and service providers

--- config/app.php
+++ config/app.php
@@ -204,7 +204,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Mapper'         => 'Cornford\Googlmapper\Facades\MapperFacade',
+        'Mapper' => 'Cornford\Googlmapper\Facades\MapperFacade',
     ],
 
 ];
