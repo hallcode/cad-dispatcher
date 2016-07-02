d7f225bfba68b16eb0465848a6a2475e9fbbe7d3
garlics93 <alexhall93@me.com>
2016-07-02 09:02:29 +0100

    Added dynamic page title to html layout

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    @if (isset($page_title))
+    <title>{{ $page_title }} | Dispatcher</title>
+    @else
     <title>Dispatcher</title>
-
+    @endif
+    
     <!-- fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 
