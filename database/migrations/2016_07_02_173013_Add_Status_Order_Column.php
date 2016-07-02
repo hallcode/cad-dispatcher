eeab05f569057dd11612ea6b4e94fff44f330f42
garlics93 <alexhall93@me.com>
2016-07-02 17:32:25 +0100

    Add order column to Statuses

---
+++ database/migrations/2016_07_02_173013_Add_Status_Order_Column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatusOrderColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('statuses', function (Blueprint $table) {
+            $table->integer('order')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('statuses', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+    }
+}
